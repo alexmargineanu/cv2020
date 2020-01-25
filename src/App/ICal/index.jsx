0ed88a060802035f34da8a2294562ac49d6c0b91
Alecu <alex.margineanu@gmail.com>
2020-01-25 12:56:07 +0000

    removed unused method from iCalParser used to dump a raw calendar

--- src/App/ICal/index.jsx
+++ src/App/ICal/index.jsx
@@ -2,35 +2,6 @@ import * as ICAL from 'ical.js';
 
 const iCalParser = (data) => {
 
-    const dumpCalendar = (parsedEvents) => {
-        const comp = new ICAL.Component(['vcalendar', [], []]);
-
-        const flightEvents = parsedEvents.filter(d => {
-            if(d.summary){
-                return d.summary.indexOf('Flight to') > -1;
-            }
-            return false;
-        });
-        flightEvents.map(f => {
-            const vevent = new ICAL.Component('vevent');
-            var event = new ICAL.Event(vevent);
-            event.summary = f.summary;
-            event.location = f.location;
-            event.uid = f.uid;
-
-            vevent.addPropertyWithValue('dtstart', f.dtstart);
-            vevent.addPropertyWithValue('dtend', f.dtend);
-            vevent.addPropertyWithValue('dtstamp', f.dtstamp);
-            vevent.addPropertyWithValue('created', f.created);
-
-            comp.addSubcomponent(vevent);
-        });
-
-        console.log(comp.toString());
-
-
-    }
-
     const flattenEvent = (e) => {
         let event = {};
         for(let i = 0; i < e[1].length; i++) {
